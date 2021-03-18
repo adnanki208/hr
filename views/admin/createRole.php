88baf678d104b07180c50797cdd6e79d68e8b49b
akramki202 <akramkiswani123@gmail.com>
2021-03-18 14:32:38 +0200

    updatesss

--- views/admin/createRole.php
+++ views/admin/createRole.php
@@ -13,7 +13,7 @@
         <div class="col-md-12 col-sm-12 col-xs-12">
             <div class="x_panel">
                 <div class="x_title">
-                    <h2>Put your information <small>correctly</small></h2>
+                    <h2>Put your information <small></small></h2>
                     <ul class="nav navbar-right panel_toolbox">
                         <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
                         </li>
